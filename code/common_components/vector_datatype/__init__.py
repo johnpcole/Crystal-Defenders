5940c618154dfc3b1389801788ab256d2732e945
Dr J P Cole <johnpcole@users.noreply.github.com>
2016-11-07 12:22:11 +0000

    Fix vector

--- code/common_components/vector_datatype/__init__.py
+++ code/common_components/vector_datatype/__init__.py
@@ -1,6 +1,1 @@
-# Version 5.1
-
-# - getinverted, getswapped, getint, getfloat, getrotated, getscaled fixed
-
-# ====================================================================================
 # Version 5.0
