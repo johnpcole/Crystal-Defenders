053b76ea4fe5314b110ee0ddc5eeca5f4aa3129b
Dr J P Cole <johnpcole@users.noreply.github.com>
2016-11-08 11:07:43 +0000

    Switch over to appinput common component

---
+++ code/controls_component/oldbuttonlibrary_component/oldbutton_subcomponent/oldbutton_module.py
@@ -0,0 +1,5 @@
+from . import button_class as ButtonClass
+
+def createbutton(buttonposition, buttondimensions, buttoncost):
+	return ButtonClass.DefineButton(buttonposition, buttondimensions, buttoncost)
+
